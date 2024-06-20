51e65c1808af813d52205cdabfac9eb59ea221bd
Arié Bénichou <arie-benichou@users.noreply.github.com>
2024-06-20 05:14:29 +0300

    enabling full capacity + refactoring

--- src/main/scala/DistinctComposites.scala
+++ src/main/scala/DistinctComposites.scala
@@ -2,33 +2,27 @@ object DistinctComposites {
 
   sealed case class DistinctComposites(numberOfBits: Long) {
 
-    private val numberOfWords =
-      (numberOfBits / 64 + (if (numberOfBits % 64 == 0) 0 else 1))
-
     assert(
-      numberOfWords <= Int.MaxValue,
-      "\n" + "number of bits can not be greater than 137 438 953 408"
+      numberOfBits <= Int.MaxValue * 64L,
+      "\n" +
+        "number of bits greater than 137 438 953 408 " +
+        "is currently not supported"
     )
 
-    private val bytes = new Array[Long](numberOfWords.toInt)
+    private val numberOfWords =
+      (numberOfBits / 64 + (if (numberOfBits % 64 == 0) 0 else 1)).toInt
+
+    private val bytes = new Array[Long](numberOfWords)
 
     lazy val capacity: Long = 64 * numberOfWords
 
     @inline
-    def set(index: Int): Unit = {
-      val bitMask = 1L << (index & 0x3f)
-      bytes(index >> 6) |= bitMask
-    }
-
-    // TODO could use fold
+    def set(index: Long): Unit = bytes((index >> 6).toInt) |= (1L << (index & 0x3f))
+    
     def cardinality(): Int = {
-      var sum = 0
-      var i = 0
-      while (i < numberOfWords) {
-        sum += java.lang.Long.bitCount(bytes(i))
-        i += 1
+      (0 until numberOfWords).foldLeft(0) { (sum, i) =>
+        sum + java.lang.Long.bitCount(bytes(i))
       }
-      sum
     }
 
   }
